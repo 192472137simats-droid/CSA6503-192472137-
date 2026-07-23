4664daee75a225cf52246cf6021d736c53873507
SAI TEJA <192472137.simats@saveetha.com>
2026-07-23 11:16:52 +0530

    Delete mat 5.py

--- mat 5.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-labels = ['CS', 'EEE', 'MECH', 'CIVIL']
-sizes = [40, 25, 20, 15]
-plt.pie(sizes, labels=labels, autopct='%1.1f%%')
-plt.title("Department Distribution")
-plt.show()
