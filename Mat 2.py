f769fd444826d76bf3dc6a1b39e0c5e69f78a31e
SAI TEJA <192472137.simats@saveetha.com>
2026-07-23 11:13:27 +0530

    Delete Mat 2.py

--- Mat 2.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-subjects = ['OS', 'DAA', 'NLP', 'RL']
-marks = [85, 90, 78, 88]
-plt.bar(subjects, marks, color='skyblue')
-plt.title("Marks by Subject")
-plt.show()
